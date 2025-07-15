9714c6bb01b4caa6de29c6f2f5d2f018670d718a
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-07-15 12:58:55 -0400

    trsj

---
+++ quiz_app/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-xl">Quiz App</h1>
+        <h2 class="text-l">Create and share your own quizzes!</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
